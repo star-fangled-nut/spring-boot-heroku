b0805b4d853562c6505bb9feb10d9e40ea42475f
Paul Gareze <paul.gareze@gmail.com>
2020-12-24 11:29:23 +0000

    Rename application

--- src/main/java/com/example/demo/Demo.java
+++ src/main/java/com/example/demo/Demo.java
@@ -8,10 +8,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
-public class DemoApplication {
+public class Demo {
 
     public static void main(String[] args) {
-        SpringApplication.run(DemoApplication.class, args);
+        SpringApplication.run(Demo.class, args);
     }
 
     @GetMapping("/hello")
